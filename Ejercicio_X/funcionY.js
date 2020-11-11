a502d4b28a196da079562343e3f3ff85c9ab5f33
Sebastián <serviciossociologicos@gmail.com>
2020-11-11 07:55:22 -0500

    Archivo ejercicio funciones

---
+++ Ejercicio_X/funcionY.js
@@ -0,0 +1,114 @@
+'use strict'
+//Ejercicio #1
+
+//Inicio
+
+//Declarar Variables
+//Nota: aplicar float;
+
+let area;
+let base;
+let altura;
+
+function Calcular(){
+
+//Leer variables
+
+//Ingresar base.
+base= parseFloat(document.getElementById('base').value);
+
+//Ingresar altura.
+altura= parseFloat(document.getElementById('altura').value);
+
+//Invocar función
+area = areaRectangulo(base,altura);
+
+//Imprimir
+
+document.getElementById('totalarea').value= area;
+
+}
+
+//Procedimiento
+
+
+function areaRectangulo(base,altura){
+
+    area = base*altura;
+    return area;
+}
+
+
+//Final
+
+//-------------------------------------------------------------------------------------------------------->
+
+//Ejercicio #2//
+
+//Inicio
+
+//Declarar Variables
+///Nota: aplicar float
+
+let radio;
+let radio2;
+let areaCirc;
+let numeroPi=3.1416;
+
+function CalcularA(){
+
+//Leer variables
+
+//Ingresar radio
+radio = Number(document.getElementById('radio').value); 
+
+
+//Invocar Función
+areaCirc = areaCircunferencia(numeroPi,radio2);
+radio2 = calculoRadio2(radio); 
+
+
+//Imprimir
+document.getElementById('totalarea2').value= numeroPi*radio2;
+
+}
+
+//Procedimiento
+
+function areaCircunferencia(numeroPi,radio2){
+
+areaCirc = numeroPi*radio2;
+return areaCirc;
+
+}
+
+function calculoRadio2(radio){
+
+    radio2 = radio*radio;
+    return radio2;
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Archivo para funciones a entregar miércoles*/
